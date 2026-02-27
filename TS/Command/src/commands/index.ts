8482ccaae00b5c8b555d87bd54d01bb94980d463
Juan Carlos Avila <48498478+JuancaJcA@users.noreply.github.com>
2026-02-27 11:06:17 -0400

    Delete TS directory

--- TS/Command/src/commands/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * BARREL EXPORT:
- * Este archivo actúa como una "ventanilla única" para la carpeta.
- * Permite que otros archivos importen múltiples comandos desde una sola ruta.
- * Ejemplo: import { AddProductCommand, ApplyDiscountCommand } from "./commands";
- */
-
-export * from "./add-product.command";
-export * from "./apply-discount.command";
